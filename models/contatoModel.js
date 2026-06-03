e556e6576059573bfbdf2df9b48e5486b7958c17
Enzo Schiavo <enzoschiavo238@gmail.com>
2026-06-03 10:00:28 -0300

    Automatic Changes

--- models/contatoModel.js
+++ models/contatoModel.js
@@ -8,73 +8,63 @@ const contatoSchema = new mongoose.Schema({
     origin: { type: String },
     personality: { type: String },
     history: { type: String },
-    wishes: { type: String },
-    required: [true, 'The caractheristics is required']
+    wishes: { type: String }
   },
   atributes: {
-    level: { type: Int8Array, required: [true, ' is required'] },
-    dp: { type: Int8Array, required: [true, ' is required'] },
-    sanity: { type: Int8Array, required: [true, ' is required'] }, 
-    pv: { type: Int8Array, required: [true, ' is required'] },
-    shield: { type: Int8Array, required: [true, ' is required'] },
-    ammo: { type: Int8Array, required: [true, ' is required'] },
-    pary: { type: Int8Array },
-    dodge: { type: Int8Array },
-    required: [true, 'The caractheristics is required']
+    level: { type: Number, required: [true, ' is required'] },
+    dp: { type: Number, required: [true, ' is required'] },
+    sanity: { type: Number, required: [true, ' is required'] }, 
+    pv: { type: Number, required: [true, ' is required'] },
+    shield: { type: Number, required: [true, ' is required'] },
+    ammo: { type: Number, required: [true, ' is required'] },
+    pary: { type: Number },
+    dodge: { type: Number }
   },
   habilities: {
     naturals: {
-      inteligence : { type: Int8Array, required: [true, 'Inteligence is required'] },
-      wisdom : { type: Int8Array, required: [true, 'Wisdom is required'] },
-      existance : { type: Int8Array, required: [true, 'Existance is required'] },
-      //required: [true, 'The caractheristics is required']
+      inteligence : { type: Number, required: [true, 'Inteligence is required'] },
+      wisdom : { type: Number, required: [true, 'Wisdom is required'] },
+      existance : { type: Number, required: [true, 'Existance is required'] }
     },
     main: {
-      intelect  : { type: Int8Array, required: [true, ' is required'] },
-      strengh   : { type: Int8Array, required: [true, ' is required'] },
-      agility   : { type: Int8Array, required: [true, ' is required'] },
-      charisma  : { type: Int8Array, required: [true, ' is required'] },
-      willing   : { type: Int8Array, required: [true, ' is required'] },
-      //required: [true, 'The caractheristics is required']
+      intelect  : { type: Number, required: [true, ' is required'] },
+      strengh   : { type: Number, required: [true, ' is required'] },
+      agility   : { type: Number, required: [true, ' is required'] },
+      charisma  : { type: Number, required: [true, ' is required'] },
+      willing   : { type: Number, required: [true, ' is required'] }
     },
     lasts: {
-      acrobacy    : { type: Int8Array, required: [true, ' is required'] },
-      art         : { type: Int8Array, required: [true, ' is required'] },
-      currents    : { type: Int8Array, required: [true, ' is required'] },
-      training    : { type: Int8Array, required: [true, ' is required'] },
-      atlethics   : { type: Int8Array, required: [true, ' is required'] },
-      science     : { type: Int8Array, required: [true, ' is required'] },
-      crime       : { type: Int8Array, required: [true, ' is required'] },
-      diplomacy   : { type: Int8Array, required: [true, ' is required'] },
-      decieving   : { type: Int8Array, required: [true, ' is required'] },
-      fortitude   : { type: Int8Array, required: [true, ' is required'] },
-      furtivity   : { type: Int8Array, required: [true, ' is required'] },
-      initiative  : { type: Int8Array, required: [true, ' is required'] },
-      intimidating: { type: Int8Array, required: [true, ' is required'] },
-      intuition   : { type: Int8Array, required: [true, ' is required'] },
-      fight       : { type: Int8Array, required: [true, ' is required'] },
-      medicine    : { type: Int8Array, required: [true, ' is required'] },
-      ocultism    : { type: Int8Array, required: [true, ' is required'] },
-      misticism   : { type: Int8Array, required: [true, ' is required'] },
-      perception  : { type: Int8Array, required: [true, ' is required'] },
-      piloting    : { type: Int8Array, required: [true, ' is required'] },
-      profession  : { type: Int8Array, required: [true, ' is required'] },
-      acuracy     : { type: Int8Array, required: [true, ' is required'] },
-      reflex      : { type: Int8Array, required: [true, ' is required'] },
-      religion    : { type: Int8Array, required: [true, ' is required'] },
-      surviving   : { type: Int8Array, required: [true, ' is required'] },
-      tatics      : { type: Int8Array, required: [true, ' is required'] },
-      technology  : { type: Int8Array, required: [true, ' is required'] },
-      force       : { type: Int8Array, required: [true, ' is required'] },
-      //required: [true, 'The caractheristics is required']
+      acrobacy    : { type: Number, required: [true, ' is required'] },
+      art         : { type: Number, required: [true, ' is required'] },
+      currents    : { type: Number, required: [true, ' is required'] },
+      training    : { type: Number, required: [true, ' is required'] },
+      atlethics   : { type: Number, required: [true, ' is required'] },
+      science     : { type: Number, required: [true, ' is required'] },
+      crime       : { type: Number, required: [true, ' is required'] },
+      diplomacy   : { type: Number, required: [true, ' is required'] },
+      decieving   : { type: Number, required: [true, ' is required'] },
+      fortitude   : { type: Number, required: [true, ' is required'] },
+      furtivity   : { type: Number, required: [true, ' is required'] },
+      initiative  : { type: Number, required: [true, ' is required'] },
+      intimidating: { type: Number, required: [true, ' is required'] },
+      intuition   : { type: Number, required: [true, ' is required'] },
+      fight       : { type: Number, required: [true, ' is required'] },
+      medicine    : { type: Number, required: [true, ' is required'] },
+      ocultism    : { type: Number, required: [true, ' is required'] },
+      misticism   : { type: Number, required: [true, ' is required'] },
+      perception  : { type: Number, required: [true, ' is required'] },
+      piloting    : { type: Number, required: [true, ' is required'] },
+      profession  : { type: Number, required: [true, ' is required'] },
+      acuracy     : { type: Number, required: [true, ' is required'] },
+      reflex      : { type: Number, required: [true, ' is required'] },
+      religion    : { type: Number, required: [true, ' is required'] },
+      surviving   : { type: Number, required: [true, ' is required'] },
+      tatics      : { type: Number, required: [true, ' is required'] },
+      technology  : { type: Number, required: [true, ' is required'] },
+      force       : { type: Number, required: [true, ' is required'] }
     },
     extras: {}
   }
-  /*
-  ,
-    items: {},
-    money: {}
-  */
 }, {
   timestamps: true
 });
